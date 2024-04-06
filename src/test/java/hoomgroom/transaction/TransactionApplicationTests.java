2ce2424cb6e2b6eac8beb0dce2dcfe0e2302eba1
Matthew <matthewhjt72@gmail.com>
2024-04-06 21:15:23 +0700

    init

---
+++ src/test/java/hoomgroom/transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package hoomgroom.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
