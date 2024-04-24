97920c6b5258a16c230c4db82ff8aaa291ae8ee4
MRadhityaUtomo <radhityautomo26@gmail.com>
2024-04-24 18:12:50 +0700

    [SPIKING] Added basic controller to access localhost.

---
+++ src/main/java/hoomgroom/transaction/controller/PengirimanController.java
@@ -0,0 +1,19 @@
+package hoomgroom.transaction.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class PengirimanController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String createUserPage(Model model) {
+        return "<h1>Test</h1>";
+    }
+}
