9e2d01ff1361e90d0ffc796c3649290600445671
MRadhityaUtomo <radhityautomo26@gmail.com>
2024-04-24 18:13:43 +0700

    [SPIKING] Added ENUMS for Pengiriman as PengirimanStatus.

---
+++ src/main/java/hoomgroom/transaction/enums/PengirimanStatus.java
@@ -0,0 +1,26 @@
+package hoomgroom.transaction.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PengirimanStatus {
+    DALAM_PROSES("DALAM_PROSES"),
+    SEDANG_DIKEMAS("SEDANG_DIKEMAS"),
+    SEDANG_DIKIRIM("SEDANG_DIKIRIM"),
+    TELAH_TIBA("TELAH_TIBA");
+
+    private final String value;
+
+    private PengirimanStatus(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains(String param) {
+        for (PengirimanStatus pengirimanStatus : PengirimanStatus.values()) {
+            if (pengirimanStatus.name().equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
