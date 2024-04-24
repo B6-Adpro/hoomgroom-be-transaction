8c796014db9da8f8c55dda63d9c19df9ffd82f79
MRadhityaUtomo <radhityautomo26@gmail.com>
2024-04-24 18:11:52 +0700

    [SPIKING] Added Pengiriman model skeleton.

---
+++ src/main/java/hoomgroom/transaction/model/Pengiriman.java
@@ -0,0 +1,17 @@
+package hoomgroom.transaction.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter @Setter
+public class Pengiriman {
+    private String pengirimanId;
+    private String alamatPengiriman;
+    private String penerimaPengiriman;
+    private String pengirimanState;
+    private String furniturePengiriman;
+    private Date tanggalPengiriman;
+}
