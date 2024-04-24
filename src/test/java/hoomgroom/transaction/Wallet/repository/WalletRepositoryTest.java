86a316b0180578d1ebe8b1d06dc1f2c9da2de97d
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-04-24 21:03:19 +0700

    added tests for Wallet model, repos, and service using strategy pattern

---
+++ src/test/java/hoomgroom/transaction/Wallet/repository/WalletRepositoryTest.java
@@ -0,0 +1,30 @@
+package hoomgroom.transaction.Wallet.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class WalletRepositoryTest {
+    @InjectMocks
+    WalletRepository walletRepository;
+    @BeforeEach
+    void SetUp() {}
+
+    @Test
+    void testAddAndFind() {
+        Wallet wallet = new Wallet();
+        wallet.setWalletId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        wallet.setBalance(100000);
+        walletRepository.addWallet(wallet);
+
+        Wallet found = walletRepository.findById("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        assertEquals(found.getWalletId(), wallet.getWalletId());
+        assertEquals(found.getBalance(), wallet.getBalance());
+    }
+
+}
---
+++ src/test/java/hoomgroom/transaction/Wallet/service/WalletServiceEWalletTest.java
@@ -0,0 +1,37 @@
+package hoomgroom.transaction.Wallet.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class WalletServiceEWalletTest {
+    @InjectMocks
+    WalletServiceEWallet walletService;
+
+    @Test
+    void testAddAndGetWallet() {
+        Wallet wallet = new Wallet();
+        wallet.setWalletId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        wallet.setBalance(100000);
+        walletService.add(wallet);
+
+        Wallet found = walletService.getWalletById("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        assertEquals(found.getWalletId(), wallet.getWalletId());
+        assertEquals(found.getBalance(), wallet.getBalance());
+    }
+
+    @Test
+    void testTopUp() {
+        Wallet wallet = new Wallet();
+        wallet.setWalletId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        wallet.setBalance(100000);
+        walletService.add(wallet);
+
+        walletService.topUp("eb558e9f-1c39-460e-8860-71af6af63bd6", 20000);
+        assertEquals(wallet.getBalance(), 120000);
+    }
+}
