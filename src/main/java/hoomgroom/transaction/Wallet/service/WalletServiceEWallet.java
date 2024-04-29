891287bddee0c811381f9fc6f015b41bbfe97899
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-04-29 21:34:21 +0700

    added validatePhoneNumber method according to international law

--- src/main/java/hoomgroom/transaction/Wallet/service/WalletServiceEWallet.java
+++ src/main/java/hoomgroom/transaction/Wallet/service/WalletServiceEWallet.java
@@ -5,6 +5,8 @@ import hoomgroom.transaction.Wallet.repository.WalletRepository;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.regex.Pattern;
+
 public class WalletServiceEWallet implements WalletService{
     @Getter
     @Setter
@@ -28,4 +30,12 @@ public class WalletServiceEWallet implements WalletService{
         wallet.setBalance(wallet.getBalance() + amount);
     }
 
+    public Boolean validatePhoneNumber(String phoneNumber) {
+        //According to the international phone numbering plan (ITU-T E. 164), phone numbers cannot contain more than 15 digits.
+        //The shortest international phone numbers in use contain seven digits
+        String phonePattern = "^\\+?[0-9]\\d{6,14}$";
+
+        Pattern pattern = Pattern.compile(phonePattern);
+        return phoneNumber != null && pattern.matcher(phoneNumber).matches();
+    }
 }
