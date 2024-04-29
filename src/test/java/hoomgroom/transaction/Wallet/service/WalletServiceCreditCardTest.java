76d8c4d920d7169d4812ef79383670f863f4c688
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-04-29 20:37:48 +0700

    added tests for ValidateCardNumber method in WalletServiceCreditCard

--- src/test/java/hoomgroom/transaction/Wallet/service/WalletServiceCreditCardTest.java
+++ src/test/java/hoomgroom/transaction/Wallet/service/WalletServiceCreditCardTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @ExtendWith(MockitoExtension.class)
 public class WalletServiceCreditCardTest {
@@ -34,5 +35,49 @@ public class WalletServiceCreditCardTest {
 
         walletService.topUp("eb558e9f-1c39-460e-8860-71af6af63bd6", 20000);
         assertEquals(wallet.getBalance(), 120000);
+    }
+    @Test
+    void tesValidateCardNumberWithValidNumber() {
+        String cardNumber = "4103601193829382";
+        walletService.setCardNumber(cardNumber);
+        assertEquals(walletService.getCardNumber(), cardNumber);
+        assertTrue(walletService.validateCardNumber(cardNumber));
+    }
+
+    @Test
+    void testValidateCardNumberWithInvalidNumberFormat() {
+        String cardNumber = "4103601193ABCDEF";
+        walletService.setCardNumber(cardNumber);
+        assertEquals(walletService.getCardNumber(), cardNumber);
+        assertFalse(walletService.validateCardNumber(cardNumber));
+    }
+
+    @Test
+    void testValidateCardNumberWithInvalidNumber() {
+        String cardNumber = "4539704384706391";
+        walletService.setCardNumber(cardNumber);
+        assertEquals(walletService.getCardNumber(), cardNumber);
+        assertFalse(walletService.validateCardNumber(cardNumber));
+    }
+
+    @Test
+    void testValidateCardAfterInvalid() {
+        String cardNumber = "4539704384706391";
+        walletService.setCardNumber(cardNumber);
+        assertEquals(walletService.getCardNumber(), cardNumber);
+        assertFalse(walletService.validateCardNumber(cardNumber));
+
+        cardNumber = "4539704384706395";
+        walletService.setCardNumber(cardNumber);
+        assertEquals(walletService.getCardNumber(), cardNumber);
+        assertTrue(walletService.validateCardNumber(cardNumber));
+    }
+
+    @Test
+    void testInvalidCreditCardNumberLength() {
+        String cardNumber = "4539704384706391";
+        walletService.setCardNumber(cardNumber);
+        assertEquals(walletService.getCardNumber(), cardNumber);
+        assertFalse(walletService.validateCardNumber(cardNumber));
     }
 }
