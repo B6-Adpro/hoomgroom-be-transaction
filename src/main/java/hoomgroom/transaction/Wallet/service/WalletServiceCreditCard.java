1cd9db47a51072cd765f5aca7c0c87d3a1137b9c
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-04-29 20:41:52 +0700

    added validateCardNumber for validating credit card with Luhn's algorithm

--- src/main/java/hoomgroom/transaction/Wallet/service/WalletServiceCreditCard.java
+++ src/main/java/hoomgroom/transaction/Wallet/service/WalletServiceCreditCard.java
@@ -27,4 +27,31 @@ public class WalletServiceCreditCard implements WalletService{
         wallet.setBalance(wallet.getBalance() + amount);
     }
 
+    public Boolean validateCardNumber(String cardNumber) {
+        // Check if cardNumberStr is not null and contains only digits
+        if (cardNumber == null || !cardNumber.matches("\\d+")) {
+            return false;
+        }
+
+        // Check if the length of cardNumberStr is exactly 16 digits
+        if (cardNumber.length() != 16) {
+            return false;
+        }
+
+        //applying Luhn's algorithm
+        int sum = 0;
+        boolean alternate = false;
+        for (int i = cardNumber.length() - 1; i >= 0; i--) {
+            int digit = Character.getNumericValue(cardNumber.charAt(i));
+            if (alternate) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit = (digit % 10) + 1;
+                }
+            }
+            sum += digit;
+            alternate = !alternate;
+        }
+        return (sum % 10 == 0);
+    }
 }
