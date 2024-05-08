c434edc642e6289f774964623ae3b13e8b522a73
MRadhityaUtomo <radhityautomo26@gmail.com>
2024-05-09 00:40:51 +0700

    [GREEN] Add controller for Pengiriman.

---
+++ src/main/java/hoomgroom/transaction/pengiriman/controller/PengirimanController.java
@@ -0,0 +1,41 @@
+package hoomgroom.transaction.pengiriman.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import hoomgroom.transaction.pengiriman.dto.PengirimanData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import hoomgroom.transaction.pengiriman.model.Pengiriman;
+import hoomgroom.transaction.pengiriman.service.PengirimanService;
+
+@RestController
+public class PengirimanController {
+
+    @Autowired
+    private PengirimanService pengirimanService;
+
+    @RequestMapping(value = "/pengiriman/view", method = RequestMethod.GET)
+    public List<PengirimanData> getAllPengiriman() {
+        return pengirimanService.getAllPengiriman();
+    }
+    @RequestMapping(value = "/pengiriman/create", method = RequestMethod.POST)
+    public ResponseEntity createPengiriman(@RequestBody Pengiriman pengiriman) {
+        ResponseEntity responseEntity = null;
+        try {
+            pengirimanService.createPengiriman(pengiriman);
+            responseEntity = ResponseEntity.ok().build();
+        } catch (Exception e) {
+            System.out.println("Error!");
+            responseEntity = ResponseEntity.badRequest().body(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return responseEntity;
+    }
+
+}
