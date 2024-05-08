15d2669c4c27e12b1931a4dbd3faded0f539ff9f
MRadhityaUtomo <radhityautomo26@gmail.com>
2024-05-08 23:57:41 +0700

    [RED] add implementation of Pengiriman model

--- src/main/java/hoomgroom/transaction/pengiriman/model/Pengiriman.java
+++ src/main/java/hoomgroom/transaction/pengiriman/model/Pengiriman.java
@@ -1,20 +1,38 @@
 package hoomgroom.transaction.pengiriman.model;
 
+import hoomgroom.transaction.pengiriman.enums.PengirimanStatus;
+import hoomgroom.transaction.pengiriman.service.State.PengirimanState;
+import hoomgroom.transaction.pengiriman.service.State.ProcessingState;
 import jakarta.persistence.*;
 import java.util.UUID;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import hoomgroom.transaction.pengiriman.enums.PengirimanStatus;
-
-@Getter @Setter
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Pengiriman")
 public class Pengiriman {
+    @Column(name = "id_transaksi")
     String transaksiId;
-    String pengirimanId;
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @Column(name = "id_pengiriman",updatable = false, nullable = false)
+    Long pengirimanId;
+
+    @Column(name = "alamat_pengiriman")
     String alamatPengiriman;
-    String penerimaPengiriman;
-    String state;
+
+    @Transient
+    PengirimanState state = new ProcessingState();
+
+    @Column(name = "state")
+    String stateString = PengirimanStatus.DALAM_PROSES.getValue();;
+
+    @Column(name = "furniture_pengiriman")
     String furniturePengiriman;
 }
