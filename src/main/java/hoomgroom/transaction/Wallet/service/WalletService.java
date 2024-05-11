76aed7e6bceebbd5a0da1a59860a80f63ef53c22
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-05-11 14:05:13 +0700

    added wallet service tests and refactor old wallet service

--- src/main/java/hoomgroom/transaction/Wallet/service/WalletService.java
+++ src/main/java/hoomgroom/transaction/Wallet/service/WalletService.java
@@ -4,19 +4,31 @@ import hoomgroom.transaction.Wallet.model.Wallet;
 import hoomgroom.transaction.Wallet.repository.WalletRepository;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Service
 @Setter
+@Getter
 public class WalletService {
-    private final WalletRepository walletRepository = new WalletRepository();
-    @Getter
-    private String method;
-    private TopUpStrategy strategy;
-    public void add(Wallet wallet) {
-        walletRepository.addWallet(wallet);
+    private final WalletRepository walletRepository;
+
+    @Autowired
+    public WalletService(WalletRepository walletRepository) {
+        this.walletRepository = walletRepository;
     }
+
     public Wallet getWalletById(String walletId) {
-        Wallet wallet = walletRepository.findById(walletId);
-        return wallet;
+        return walletRepository.findById(walletId)
+                .orElse(null);
+    }
+
+    private TopUpStrategy strategy;
+    public Wallet add() {
+        Wallet wallet = new Wallet();
+        return walletRepository.save(wallet);
     }
 
     public void topUp(String walletId, double amount) {
--- src/test/java/hoomgroom/transaction/Wallet/service/WalletServiceTest.java
+++ src/test/java/hoomgroom/transaction/Wallet/service/WalletServiceTest.java
@@ -8,11 +8,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class WalletServiceTest {
+
     @Mock
     WalletRepository walletRepository;
 
@@ -24,11 +28,12 @@ public class WalletServiceTest {
         Wallet wallet = new Wallet();
         wallet.setWalletId("eb558e9f-1c39-460e-8860-71af6af63bd6");
         wallet.setBalance(100000);
-        walletService.add(wallet);
+        walletRepository.save(wallet);
 
-        Wallet found = walletService.getWalletById("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        assertEquals(found.getWalletId(), wallet.getWalletId());
-        assertEquals(found.getBalance(), wallet.getBalance());
+        Optional<Wallet> found = walletRepository.findById("eb558e9f-1c39-460e-8860-71af6af63bd6");
+
+        assertThat(found.isPresent());
+        assertEquals(wallet, found.get());
     }
 
     @Test
@@ -36,7 +41,7 @@ public class WalletServiceTest {
         Wallet wallet = new Wallet();
         wallet.setWalletId("eb558e9f-1c39-460e-8860-71af6af63bd6");
         wallet.setBalance(100000);
-        when(walletRepository.findById("eb558e9f-1c39-460e-8860-71af6af63bd6")).thenReturn(wallet);
+        when(walletRepository.findById("eb558e9f-1c39-460e-8860-71af6af63bd6")).thenReturn(java.util.Optional.of(wallet));
 
         String cardNumber = "4103601193829382";
 
@@ -48,4 +53,22 @@ public class WalletServiceTest {
 
         assertEquals(150000, wallet.getBalance());
     }
+
+//    @Test
+//    void testTopUpWithInvalidCreditCard() {
+//        Wallet wallet = new Wallet();
+//        wallet.setWalletId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+//        wallet.setBalance(100000);
+//        when(walletRepository.findById("eb558e9f-1c39-460e-8860-71af6af63bd6")).thenReturn(java.util.Optional.of(wallet));
+//
+//        String cardNumber = "invalidCardNumber";
+//
+//        TopUpByCreditCard strategy = new TopUpByCreditCard(walletRepository);
+//        strategy.setCardNumber(cardNumber);
+//        walletService.setStrategy(strategy);
+//
+//        walletService.topUp("eb558e9f-1c39-460e-8860-71af6af63bd6", 50000);
+//
+//        assertEquals(100000, wallet.getBalance()); // Balance should remain unchanged
+//    }
 }
