1af20b840eee62474e22f0433e7aab5a12c392bd
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-05-11 14:09:18 +0700

    added the base interface for topUpStrategy

--- src/main/java/hoomgroom/transaction/Wallet/service/TopUpStrategy.java
+++ src/main/java/hoomgroom/transaction/Wallet/service/TopUpStrategy.java
@@ -2,6 +2,8 @@ package hoomgroom.transaction.Wallet.service;
 
 import org.springframework.boot.autoconfigure.batch.BatchDataSourceScriptDatabaseInitializer;
 
+import java.util.UUID;
+
 public interface TopUpStrategy {
     public boolean validateTopUpDetails();
     public void topUp(String walletId, double amount);
