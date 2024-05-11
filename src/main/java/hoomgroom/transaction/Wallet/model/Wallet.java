861c84f724d7a44d07efd9738f2998d381257fd0
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-05-11 14:01:54 +0700

    refactor wallet to become Entity

--- src/main/java/hoomgroom/transaction/Wallet/model/Wallet.java
+++ src/main/java/hoomgroom/transaction/Wallet/model/Wallet.java
@@ -1,17 +1,23 @@
 package hoomgroom.transaction.Wallet.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.UUID;
 
+@Entity
 @Getter
 @Setter
 public class Wallet {
+
+    @Id
     private String walletId;
+
     private double balance;
 
     public Wallet() {
-        this.walletId = UUID.randomUUID().toString();
+        this.walletId = UUID.randomUUID().toString(); // Optionally, generate UUID during object creation
     }
 }
