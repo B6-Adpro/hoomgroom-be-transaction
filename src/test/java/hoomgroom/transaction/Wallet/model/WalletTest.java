afa2d9c277997e7cdde2718367595a19cbd9f582
SkyClave <alwindjuliansah07@gmail.com>
2024-05-26 23:20:15 +0700

    add deleted folder

---
+++ src/test/java/hoomgroom/transaction/Wallet/model/WalletTest.java
@@ -0,0 +1,22 @@
+package hoomgroom.transaction.Wallet.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WalletTest {
+    Wallet wallet;
+    @BeforeEach
+    void SetUp() {
+        this.wallet = new Wallet();
+        this.wallet.setWalletId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        this.wallet.setBalance(100000);
+    }
+
+    @Test
+    void testGetWalletBalance() {
+        assertEquals(100000, this.wallet.getBalance());
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.wallet.getWalletId());
+    }
+}
---
+++ src/test/java/hoomgroom/transaction/Wallet/repository/WalletRepositoryTest.java
@@ -0,0 +1,90 @@
+package hoomgroom.transaction.Wallet.repository;
+
+import hoomgroom.transaction.Wallet.model.Wallet;
+import hoomgroom.transaction.Wallet.repository.WalletRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class WalletRepositoryTest {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    @Test
+    public void testSaveWallet() {
+        // Given
+        Wallet wallet = new Wallet();
+        wallet.setWalletId(UUID.randomUUID().toString());
+        wallet.setBalance(0.0);
+
+        // When
+        Wallet savedWallet = walletRepository.save(wallet);
+
+        // Then
+        assertThat(savedWallet).isNotNull();
+        assertThat(savedWallet.getWalletId()).isNotNull();
+        assertEquals(savedWallet.getBalance(), 0.0);
+    }
+
+    @Test
+    public void testFindById() {
+        Wallet wallet = new Wallet();
+        wallet.setWalletId(UUID.randomUUID().toString());
+        wallet.setBalance(100.0);
+        walletRepository.save(wallet);
+
+        Optional<Wallet> foundWallet = walletRepository.findById(wallet.getWalletId());
+
+        assertThat(foundWallet).isPresent();
+        assertEquals(foundWallet.get().getWalletId(), wallet.getWalletId());
+        assertEquals(foundWallet.get().getBalance(), wallet.getBalance());
+    }
+
+    @Test
+    public void testUpdateWallet() {
+        Wallet wallet = new Wallet();
+        wallet.setWalletId(UUID.randomUUID().toString());
+        wallet.setBalance(300.0);
+        walletRepository.save(wallet);
+
+        wallet.setBalance(400.0);
+        Wallet updatedWallet = walletRepository.save(wallet);
+
+        assertThat(updatedWallet).isNotNull();
+        assertEquals(updatedWallet.getWalletId(), wallet.getWalletId());
+        assertEquals(updatedWallet.getBalance(), wallet.getBalance());
+    }
+
+    @Test
+    public void testFindIfMoreThanOne() {
+        Wallet wallet = new Wallet();
+        wallet.setWalletId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        wallet.setBalance(600.0);
+        walletRepository.save(wallet);
+
+        Wallet wallet2 = new Wallet();
+        wallet2.setWalletId(UUID.randomUUID().toString());
+        wallet2.setBalance(500.0);
+        walletRepository.save(wallet2);
+
+        Optional<Wallet> foundWallet = walletRepository.findById("eb558e9f-1c39-460e-8860-71af6af63bd6");
+
+        assertThat(foundWallet).isPresent();
+        assertNotEquals(wallet2.getWalletId(), foundWallet.get().getWalletId());
+        assertNotEquals(wallet2.getBalance(), foundWallet.get().getBalance());
+
+        assertEquals(wallet.getWalletId(), foundWallet.get().getWalletId());
+        assertEquals(wallet.getBalance(), foundWallet.get().getBalance());
+    }
+}
